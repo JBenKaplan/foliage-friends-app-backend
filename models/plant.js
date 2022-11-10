75af3cb7aa1649e380029dd21d2f9cb20213e0cb
Ben Kaplan <bjminkaplan@gmail.com>
2022-11-10 15:31:33 -0600

    Setup models with associations

--- models/plant.js
+++ models/plant.js
@@ -2,13 +2,13 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Plant extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Plant.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+      Plant.belongsTo(models.Room, {
+        foreignKey: 'roomId'
+      })
     }
   }
   Plant.init(
@@ -17,8 +17,22 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       image: DataTypes.STRING,
       details: DataTypes.STRING,
-      userId: DataTypes.INTEGER,
-      roomId: DataTypes.INTEGER
+      userId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      roomId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      }
     },
     {
       sequelize,
--- models/room.js
+++ models/room.js
@@ -2,19 +2,26 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Room extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Room.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+      Room.hasMany(models.Plant, {
+        foreignKey: 'roomId'
+      })
     }
   }
   Room.init(
     {
       name: DataTypes.STRING,
-      userId: DataTypes.INTEGER
+      userId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      }
     },
     {
       sequelize,
