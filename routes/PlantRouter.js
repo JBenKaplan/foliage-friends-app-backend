0b68651c82869787122b7d5f5ca3ba8e403cbf5b
Ben Kaplan <bjminkaplan@gmail.com>
2022-11-15 15:05:41 -0600

    Update room controller updated

--- routes/PlantRouter.js
+++ routes/PlantRouter.js
@@ -3,20 +3,34 @@ const controller = require('../controllers/PlantController.js')
 const middleware = require('../middleware')
 
 Router.get('/all', controller.GetPlants)
-Router.get('/:user_id/', controller.GetPlantByUser)
-Router.get('/:room_id/', controller.GetPlantByRoom)
+Router.get(
+  '/:user_id/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetPlantByUser
+)
+
+Router.get(
+  '/:room_id/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetPlantByRoom
+)
+
 Router.post(
   '/create',
   middleware.stripToken,
   middleware.verifyToken,
   controller.CreatePlant
 )
+
 Router.put(
   '/updateplant',
   middleware.stripToken,
   middleware.verifyToken,
   controller.UpdatePlant
 )
+
 Router.delete(
   '/:user_id',
   middleware.stripToken,
